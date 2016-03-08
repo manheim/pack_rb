fb9bb6c3103c4a99f8eb396cb2c8794b5751e821
Jonathan Niesen <jon.niesen@gmail.com>
2016-03-07 23:10:37 -0500

    fix(cmd_tests): ensure constant is declared once

--- spec/pack_rb/sub_commands/inspect_spec.rb
+++ spec/pack_rb/sub_commands/inspect_spec.rb
@@ -5,11 +5,11 @@ module PackRb
   class SubCommands
     describe Inspect do
       before do
-        Harness = Class.new { include Inspect }
+        @harness ||= Class.new { include Inspect }
       end
 
       context '#inspect_tpl' do
-        subject { Harness.new().inspect_tpl(base_cmd: 'packer') }
+        subject { @harness.new().inspect_tpl(base_cmd: 'packer') }
         it { is_expected.to eq('packer inspect') }
       end
     end
--- spec/pack_rb/sub_commands/validate_spec.rb
+++ spec/pack_rb/sub_commands/validate_spec.rb
@@ -5,11 +5,11 @@ module PackRb
   class SubCommands
     describe Validate do
       before do
-        Harness = Class.new { include Validate }
+        @harness ||= Class.new { include Validate }
       end
 
       context '#validate' do
-        subject { Harness.new().validate(base_cmd: 'packer') }
+        subject { @harness.new().validate(base_cmd: 'packer') }
         it { is_expected.to eq('packer validate') }
       end
     end
